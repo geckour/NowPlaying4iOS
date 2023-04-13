f5f18bd531595473173f60738c778c2b5bd0a7b2
geckour <geckour@gmail.com>
2023-04-13 23:33:49 +0900

    Initial Commit

---
+++ NowPlaying4iOS/NowPlaying4iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NowPlaying4iOSApp.swift
+//  NowPlaying4iOS
+//
+//  Created by geckour on 2023/04/13.
+//
+
+import SwiftUI
+
+@main
+struct NowPlaying4iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
